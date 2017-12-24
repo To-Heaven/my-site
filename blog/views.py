5eed027d4359712d9ab2cf5de85b60348d97a79e
ZiaWang <ziawang_python@163.com>
2017-12-24 12:08:18 +0800

    ADD: 新增设置cookie用来统计网站访问量

--- blog/views.py
+++ blog/views.py
@@ -5,21 +5,23 @@ from blog import models
 
 
 def home(request):
-    """ 网站主界面视图函数
-
-    """
+    """  网站主界面视图函数 """
 
     site = models.Site.objects.all().first()
-    return render(request, 'home.html', {"site": site})
-
-
-def base(request):
-
-    return render(request, 'base.html')
+    response = render(request, 'home.html', {"site": site})
+    response.set_cookie(
+        key='has_counted',
+        value='OK',
+        max_age=300,
+    )
+    return response
 
 
 def blog(request):
+    """ 博客主界面视图函数 """
     search_key = request.GET.dict()
+    if search_key.get('page'):              # 检索数据库内容时，不能包括page分页
+        del search_key['page']
     articles = models.Article.objects.filter(**search_key)
     page_obj = pagintator.Paingator(
         request=request,
@@ -33,6 +35,11 @@ def blog(request):
 
 
 def blog_category(request, site_category=None):
+    """ 文章类型页面的视图函数
+    Args:
+        site_category: 文章类型
+    """
+
     articles = models.Article.objects.filter(article_category__site_category__site_category_title=site_category)
     article_category_list = models.ArticleCategory.objects.filter(site_category__site_category_title=site_category)
     page_obj = pagintator.Paingator(
@@ -49,6 +56,10 @@ def blog_category(request, site_category=None):
 
 
 def blog_article(request, article_id=None):
+    """ 文章页面视图函数
+    Args:
+        article_id: 文章id
+    """
     article = models.Article.objects.filter(id=article_id).first()
     site_category = article.article_category.site_category
     article_category_list = article.article_category._meta.model.objects.filter(site_category=site_category)
